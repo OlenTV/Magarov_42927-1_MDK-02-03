fe5fe4e7b1e664870f84d04511f90915d8ff5aea
OlenTV <gmagarov@mail.ru>
2021-04-05 16:58:46 +0300

    Switch to fixed version
    git clone switched to fixed version of requested repos

--- BUILD.bat
+++ BUILD.bat
@@ -3,8 +3,8 @@ git symbolic-ref --short -q HEAD > version
 
 rmdir "Project" /s /q
 mkdir "Project/"
-git clone https://github.com/OlenTV/my-trpo-core-cpp.git "Project/Core"
-git clone https://github.com/OlenTV/Magarov.git "Project/Magarov"
+git clone https://github.com/OlenTV/my-trpo-core-cpp.git "Project/Core" --branch 1.0
+git clone https://github.com/OlenTV/Magarov.git "Project/Magarov" --branch 1.0
 move Source.cpp "./Project"
 
 rmdir build /s /q
