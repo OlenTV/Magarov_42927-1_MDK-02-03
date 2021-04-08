a3ca5bf6a38cfd00cb91be11c66215685a215ff5
OlenTV <gmagarov@mail.ru>
2021-04-08 21:43:45 +0300

    Lab11

--- BUILD.bat
+++ BUILD.bat
@@ -4,7 +4,7 @@ git symbolic-ref --short -q HEAD > version
 rmdir "Project" /s /q
 mkdir "Project/"
 git clone https://github.com/OlenTV/my-trpo-core-cpp.git "Project/Core" --branch 1.0
-git clone https://github.com/OlenTV/Magarov.git "Project/Magarov" --branch 1.0
+git clone https://github.com/OlenTV/Magarov.git "Project" --branch 1.0
 move Source.cpp "./Project"
 
 rmdir build /s /q
--- Source.cpp
+++ Source.cpp
@@ -1,48 +1,58 @@
 #pragma once
 #include "Magarov/QuadEquation.h"
+#include "Tests/LinEquationTest.h"
+#include "Tests/QuadEquationTest.h"
+#include "Tests/MyLogTest.h"
 using namespace Magarov;
 
 int main()
 {
-    ifstream verfile;
-    verfile.open("..\\version", fstream::in);
-    string ver;
-    verfile >> ver;
-    mylog.log("Program version is " + ver);
-    verfile.close();
+	LinEquationTest* LinEquTest = new LinEquationTest();
+	QuadEquationTest* QuadEquTest = new QuadEquationTest();
+	MyLogTest* MyLoTest = new MyLogTest();
+	mylog.log("END_TEST");
+	mylog.write();
+	mylog.clear();
 
-    float A, B, C;
-    A = B = C = 0;
-    cout << "Enter A B C" << endl;
-    cin >> A >> B >> C;
-    mylog.log(to_string(A) + "x^2 + " + to_string(B) + "x + " + to_string(C) + " = 0");
+	ifstream verfile;
+	verfile.open("..\\version", fstream::in);
+	string ver;
+	verfile >> ver;
+	mylog.log("Program version is " + ver);
+	verfile.close();
 
-    try
-    {
-        if (A != 0)
-        {
-            mylog.log("Quadratic equation");
-        }
-        else
-        {
-            mylog.log("Linear equation");
-        }
-        QuadEquation* equ = new QuadEquation();
-        vector <float> *results = equ->solve(A, B, C);
-        string str = "Roots: ";
-        for (auto const& s: *results)
-        {
-            str += to_string(s) + " ";
-        }
-        mylog.log(str);
-    }
-    catch (MagarovException& exception)
-    {
-        cout << exception.getText();
-    }
-    mylog.write();
-    mylog.clear();
+	float A, B, C;
+	A = B = C = 0;
+	cout << "Enter A B C" << endl;
+	cin >> A >> B >> C;
+	mylog.log(to_string(A) + "x^2 + " + to_string(B) + "x + " + to_string(C) + " = 0");
 
-    system("pause");
-    return NULL;
+	try
+	{
+		if (A != 0)
+		{
+			mylog.log("Quadratic equation");
+		}
+		else
+		{
+			mylog.log("Linear equation");
+		}
+		QuadEquation* equ = new QuadEquation();
+		vector <float> *results = equ->solve(A, B, C);
+		string str = "Roots: ";
+		for (auto const& s: *results)
+		{
+			str += to_string(s) + " ";
+		}
+		mylog.log(str);
+	}
+	catch (MagarovException& exception)
+	{
+		cout << exception.getText();
+	}
+	mylog.write();
+	mylog.clear();
+
+	system("pause");
+	return NULL;
 }
